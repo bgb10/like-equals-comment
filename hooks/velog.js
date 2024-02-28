14fbbebffc448e4a6a8bef4d0c1d4aff42e59b89
bgb10 <bgb10@naver.com>
2024-02-28 18:37:43 +0900

    change velog logic to hotkey

--- hooks/velog.js
+++ hooks/velog.js
@@ -1,23 +1,30 @@
-;(async () => {
+function like() {
   const like = document.querySelector('div[data-testid="like"]')
 
-  if (like) {
-    like.addEventListener('click', async (e) => {
-      if (like.getAttribute('active') == 'true') return
+  like.click()
+}
 
-      let textArea = document.querySelector(
-        '#root > div.sc-dPiLbb.sc-bBHHxi.kTIDXm > div.sc-TBWPX.dXONqK.sc-brSvTw.cgYvDI > div > div.sc-evcjhq.BdYrx > textarea'
-      )
-      textArea.focus()
+function submit() {
+  let submitButton = document.querySelector(
+    '#root > div.sc-dPiLbb.sc-bBHHxi.kTIDXm > div.sc-TBWPX.dXONqK.sc-brSvTw.cgYvDI > div > div.sc-evcjhq.BdYrx > div > button'
+  )
 
-      // WARNING: non blocking function!
-      const { comment } = await chrome.storage.local.get(['comment'])
+  submitButton.click()
+}
 
-      document.execCommand('insertText', false, comment)
-      let submitButton = document.querySelector(
-        '#root > div.sc-dPiLbb.sc-bBHHxi.kTIDXm > div.sc-TBWPX.dXONqK.sc-brSvTw.cgYvDI > div > div.sc-evcjhq.BdYrx > div > button'
-      )
-      submitButton.click()
-    })
-  }
-})()
+function comment() {
+  let textArea = document.querySelector(
+    '#root > div.sc-dPiLbb.sc-bBHHxi.kTIDXm > div.sc-TBWPX.dXONqK.sc-brSvTw.cgYvDI > div > div.sc-evcjhq.BdYrx > textarea'
+  )
+  textArea.focus()
+
+  document.execCommand('insertText', false, 'test')
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  like()
+
+  comment()
+
+  submit()
+})
