1b615b03f0dee9325e1a85ac5713db357383dd78
bgb10 <bgb10@naver.com>
2024-02-28 18:38:26 +0900

    refactor youtube hook

--- hooks/youtube.js
+++ hooks/youtube.js
@@ -1,23 +1,10 @@
-function comment() {}
-
 function like() {
   const like = document.querySelector(
     '#top-level-buttons-computed > segmented-like-dislike-button-view-model > yt-smartimation > div > div > like-button-view-model > toggle-button-view-model > button-view-model > button'
   )
-  console.log(like)
+
   like.click()
 }
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension')
-  if (request.greeting === 'hello') sendResponse({ farewell: 'goodbye' })
-
-  like()
-
-  window.scrollTo(0, 9999)
-
-  setTimeout(comment, 1000)
-})
 
 function submit() {
   let submitButton = document.querySelector('#submit-button > yt-button-shape > button')
@@ -30,16 +17,20 @@ function submit() {
 function comment() {
   let placeholder = document.querySelector('#placeholder-area')
 
-  if (placeholder) {
-    placeholder.click()
-    let textArea = document.querySelector('#contenteditable-root')
-    textArea.focus()
+  placeholder.click()
+  let textArea = document.querySelector('#contenteditable-root')
+  textArea.focus()
 
-    const { comment } = { comment: 'thank you for good video!' }
-    document.execCommand('insertText', false, comment)
-
-    setTimeout(submit, 200)
-  } else {
-    alert('no exists!')
-  }
+  const { comment } = { comment: 'thank you for good video!' }
+  document.execCommand('insertText', false, comment)
 }
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  like()
+
+  window.scrollTo(0, 9999)
+
+  setTimeout(comment, 1000)
+
+  setTimeout(submit, 200)
+})
